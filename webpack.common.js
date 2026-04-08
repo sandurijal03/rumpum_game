d9cb769478dcc183d6be475ea5bae0b576896072
sandip rijal <sandurijal03@hotmail.com>
2026-04-08 20:02:28 +0545

    refactor: update webpack configuration and add 404 error page

---
+++ webpack.common.js
@@ -0,0 +1,69 @@
+const path = require("path");
+const HtmlWebapackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.tsx",
+  output: {
+    path: path.resolve(__dirname, "build"),
+    filename: "[name].[contenthash:8].js",
+    chunkFilename: "[name].[contenthash:8].js",
+    assetModuleFilename: "assets/[name][ext]",
+    publicPath: "/",
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts)x?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "ts-loader",
+          options: {
+            transpileOnly: true,
+          },
+        },
+      },
+      {
+        test: /\.(ts|tsx|js|jsx)?$/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: [
+              "@babel/preset-env",
+              "@babel/preset-react",
+              "@babel/preset-typescript",
+            ],
+          },
+        },
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg|pdf)$/i,
+        type: "asset/resource",
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        type: "asset/resource",
+      },
+      {
+        test: /\.(s?css)$/i,
+        use: ["style-loader", "css-loader"],
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js", ".jsx"],
+  },
+  plugins: [
+    new HtmlWebapackPlugin({ template: "./public/index.html" }),
+    new HtmlWebapackPlugin({
+      template: "./public/404.html",
+      filename: "404.html",
+      inject: false,
+    }),
+  ],
+  optimization: {
+    usedExports: true,
+  },
+};
